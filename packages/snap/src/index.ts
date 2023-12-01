8b39005405e00beb2db2ea7c5ecc7b53d18816b7
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-12-01 23:49:59 +0330

    feat: read contract source code

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,26 +1,41 @@
 import type { OnTransactionHandler } from '@metamask/snaps-sdk';
 import { copyable, divider, heading, panel, text } from '@metamask/snaps-sdk';
+import type { GetContractResponse } from 'src/types';
 
-// Handle outgoing transactions.
+const apiKey = 'BQPXIFRXIU9GPFUNEVRNT34YNN39WEDPME';
 export const onTransaction: OnTransactionHandler = async ({
   transaction,
   chainId,
 }) => {
   const txData = transaction?.data;
 
-  // Display Token Audit details.
-  if (
-    !(
-      (typeof transaction.data === 'string' && transaction.data === '0x') ||
-      txData === undefined
-    )
-  ) {
+  // TODO: only check Polygon contracts
+  if (typeof txData === 'string' && txData !== '0x') {
+    const contractDataResponse = await fetch(
+      `https://api.polygonscan.com/api?module=contract&action=getsourcecode&address=${transaction.to}&apikey=${apiKey}`,
+    );
+    let json: GetContractResponse = await contractDataResponse.json();
+    while (json.result[0]?.Proxy === '1') {
+      const contractImplementationResponse = await fetch(
+        `https://api.polygonscan.com/api?module=contract&action=getsourcecode&address=${json.result[0].Implementation}&apikey=${apiKey}`,
+      );
+      json = await contractImplementationResponse.json();
+    }
+    const sourceCode = json.result[0]?.SourceCode;
+    if (!sourceCode) {
+      return {
+        content: panel([
+          heading(`Warning`),
+          text(`Contract source code is not verified`),
+        ]),
+      };
+    }
     return {
       content: panel([
         heading(`TxAi`),
         text(`Some Insight ${chainId}`),
         divider(),
-        text(`Visit:`),
+        text(sourceCode.slice(0, 100)),
         copyable(`https://www.google.com`),
       ]),
     };
---
+++ packages/snap/src/types.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export type GetContractResponse = {
+  status: string;
+  message: string;
+  result: [
+    {
+      SourceCode: string;
+      ABI: string;
+      ContractName: string;
+      CompilerVersion: string;
+      OptimizationUsed: string;
+      Runs: string;
+      ConstructorArguments: string;
+      EVMVersion: string;
+      Library: string;
+      LicenseType: string;
+      Proxy: string;
+      Implementation: string;
+      SwarmSource: string;
+    },
+  ];
+};
