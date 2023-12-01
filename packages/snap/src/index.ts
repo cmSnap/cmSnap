7c471b48f565b3025782bb841a6be9d2fa0bcbde
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-12-02 00:39:38 +0330

    feat: show full tx call data

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,6 +1,6 @@
 import { Interface } from '@ethersproject/abi';
 import type { OnTransactionHandler } from '@metamask/snaps-sdk';
-import { copyable, divider, heading, panel, text } from '@metamask/snaps-sdk';
+import { heading, panel, text } from '@metamask/snaps-sdk';
 import type { GetContractResponse } from 'src/types';
 
 const InputDataDecoder = require('ethereum-input-data-decoder');
@@ -8,7 +8,9 @@ const InputDataDecoder = require('ethereum-input-data-decoder');
 const apiKey = 'BQPXIFRXIU9GPFUNEVRNT34YNN39WEDPME';
 
 export type DecodedCall = {
-  inputs: any;
+  inputs: any[];
+  types: string[];
+  names: string[];
   method: string;
 };
 
@@ -19,16 +21,13 @@ export type DecodedCall = {
  */
 function decodeFunctionCall(abi: any[], input: string): DecodedCall {
   const decoder = new InputDataDecoder(abi);
-  const decoded = decoder.decodeData(input);
-  let { method } = decoded;
-  if (abi.filter((func) => func.name === method).length > 1) {
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    method = `${method}(${decoded.types.join(',')})`;
-  }
+  const { method, names, types } = decoder.decodeData(input);
   const iface = new Interface(abi);
   return {
     method,
-    inputs: iface.decodeFunctionData(method, input),
+    inputs: iface.decodeFunctionData(method, input) as any[],
+    types,
+    names,
   };
 }
 
@@ -38,40 +37,42 @@ export const onTransaction: OnTransactionHandler = async ({
 }) => {
   const txData = transaction?.data;
 
-  // TODO: only check Polygon contracts
-  if (typeof txData === 'string' && txData !== '0x') {
-    const contractDataResponse = await fetch(
-      `https://api.polygonscan.com/api?module=contract&action=getsourcecode&address=${transaction.to}&apikey=${apiKey}`,
+  if (typeof txData !== 'string' || txData === '0x') {
+    return null;
+  }
+  if (!chainId.endsWith('137')) {
+    return {
+      content: panel([heading(`Only on polygon`)]),
+    };
+  }
+  const contractDataResponse = await fetch(
+    `https://api.polygonscan.com/api?module=contract&action=getsourcecode&address=${transaction.to}&apikey=${apiKey}`,
+  );
+  let json: GetContractResponse = await contractDataResponse.json();
+  while (json.result[0]?.Proxy === '1') {
+    const contractImplementationResponse = await fetch(
+      `https://api.polygonscan.com/api?module=contract&action=getsourcecode&address=${json.result[0].Implementation}&apikey=${apiKey}`,
     );
-    let json: GetContractResponse = await contractDataResponse.json();
-    while (json.result[0]?.Proxy === '1') {
-      const contractImplementationResponse = await fetch(
-        `https://api.polygonscan.com/api?module=contract&action=getsourcecode&address=${json.result[0].Implementation}&apikey=${apiKey}`,
-      );
-      json = await contractImplementationResponse.json();
-    }
-    const sourceCode = json.result[0]?.SourceCode;
-    if (!sourceCode) {
-      return {
-        content: panel([
-          heading(`Warning`),
-          text(`Contract source code is not verified`),
-        ]),
-      };
-    }
-    const { method } = decodeFunctionCall(
-      JSON.parse(json.result[0].ABI),
-      txData,
-    );
+    json = await contractImplementationResponse.json();
+  }
+  const result = json.result[0];
+  const sourceCode = result.SourceCode;
+  if (!sourceCode) {
     return {
       content: panel([
-        heading(`Method: ${method}`),
-        text(`Some Insight ${chainId}`),
-        divider(),
-        text(sourceCode.slice(0, 100)),
-        copyable(`https://www.google.com`),
+        heading(`Warning`),
+        text(`Contract source code is not verified`),
       ]),
     };
   }
-  return null;
+  const abi = JSON.parse(json.result[0].ABI);
+  const { method, inputs, names } = decodeFunctionCall(abi, txData);
+  return {
+    content: panel([
+      heading(`Contract Name: ${result.ContractName}`),
+      heading(`Method: ${method}`),
+      heading(`Arguments`),
+      ...names.map((name, i) => text(`${name}: ${String(inputs[i])}`)),
+    ]),
+  };
 };
