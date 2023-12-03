656985489cf0779d30fa69d601b458c13dd872cd
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-12-03 14:06:42 +0330

    feat: add github url

--- packages/site/src/components/Footer.tsx
+++ packages/site/src/components/Footer.tsx
@@ -10,23 +10,16 @@ const FooterWrapper = styled.footer`
   border-top: 1px solid ${(props) => props.theme.colors.border?.default};
 `;
 
-const PoweredByButton = styled.a`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  padding: 1.2rem;
-  border-radius: ${({ theme }) => theme.radii.button};
-  box-shadow: ${({ theme }) => theme.shadows.button};
-  background-color: ${({ theme }) => theme.colors.background?.alternative};
-`;
-
-const PoweredByContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 1rem;
-`;
-
 export const Footer = () => {
-  return <FooterWrapper>GitHub</FooterWrapper>;
+  return (
+    <FooterWrapper>
+      <a
+        href="https://github.com/alimahdiyar/contract-method-snap"
+        target="_blank"
+        style={{ color: 'white' }}
+      >
+        GitHub
+      </a>
+    </FooterWrapper>
+  );
 };
