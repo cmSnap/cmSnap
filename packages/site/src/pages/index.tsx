567457f65d650a02392661663ff3a332013c704a
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-12-03 15:31:49 +0330

    feat: added sample scam frontend

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
+import { Web3 } from 'web3';
 
 import { explorerUrls } from '../../../snap/src/explorer';
 import {
@@ -10,6 +11,7 @@ import {
   ReconnectButton,
 } from '../components';
 import { defaultSnapOrigin } from '../config';
+import { USDT_ABI } from '../config/abis';
 import { MetamaskActions, MetaMaskContext } from '../hooks';
 import {
   connectSnap,
@@ -137,6 +139,54 @@ const Index = () => {
       console.error(error);
       dispatch({ type: MetamaskActions.SetError, payload: error });
     }
+  };
+  const sendUSDT = async () => {
+    await window.ethereum.enable();
+    const web3 = new Web3(window.ethereum);
+    const [account] = await web3.eth.getAccounts();
+    if (!account) {
+      // eslint-disable-next-line no-alert
+      alert('Wallet not connected');
+      return;
+    }
+
+    // Get chain id
+    const chainId = await web3.eth.getChainId();
+
+    // If the current network is not polygon
+    if (chainId !== 137n) {
+      const networkConfig = {
+        chainId: '0x89',
+        chainName: 'Polygon Mainnet',
+        rpcUrls: ['https://polygon-rpc.com/'],
+        nativeCurrency: {
+          name: 'MATIC',
+          symbol: 'MATIC',
+          decimals: 18,
+        },
+      };
+
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [networkConfig],
+      });
+    }
+
+    const tokenAddress = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174';
+
+    const abi = USDT_ABI;
+
+    const receiver = '0x0000000000000000000000000000000000000000';
+
+    const contract = new web3.eth.Contract(abi, tokenAddress, {
+      from: account,
+    });
+
+    const amount = '1';
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    contract?.methods?.transfer(receiver, amount)?.send().catch(console.log);
   };
 
   return (
@@ -291,6 +341,29 @@ const Index = () => {
             ),
           }}
           disabled={!state.installedSnap}
+        />
+      </CardContainer>
+      <CardContainer>
+        <Card
+          content={{
+            title: 'Sample SCAM',
+            description: (
+              <div style={{ fontWeight: 'bold' }}>
+                <p style={{ color: 'red' }}>
+                  Warning: this is a sample scam to show the usage of the snap.
+                  Press the button but DO NOT approve the transaction
+                </p>
+                <p>Press the button below to mint my NFT and win a prize!😍</p>
+              </div>
+            ),
+            button: (
+              <Button onClick={sendUSDT} disabled={!state.installedSnap}>
+                Mint NFT
+              </Button>
+            ),
+          }}
+          disabled={!state.installedSnap}
+          fullWidth={true}
         />
       </CardContainer>
     </Container>
