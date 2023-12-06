671286a670e2a45fd06f4075a8c619f0956bfe84
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-12-06 15:15:52 +0330

    fix: env file

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -1,3 +1,3 @@
 export const defaultSnapOrigin =
   // eslint-disable-next-line no-restricted-globals
-  process.env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
+  process.env.REACT_APP_SNAP_ORIGIN ?? `local:http://localhost:8080`;
