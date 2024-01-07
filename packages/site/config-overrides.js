6832776f5411bab6b6f3e1aa74439ccab0cf6e3c
Ali Mahdiyar <alimahdiyar77@gmail.com>
2024-01-07 21:19:07 +0330

    refactor: remove unnecessary config-overrides content

--- packages/site/config-overrides.js
+++ packages/site/config-overrides.js
@@ -1,8 +1,1 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const webpack = require('webpack');
-
-module.exports = {
-  webpack(config) {
-    return config;
-  },
-};
+module.exports = {};
